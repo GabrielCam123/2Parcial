6a3b00b8eb3d544c08263442bcdd546ff1e190c4
OscarCameo <cameotapia@gmail.com>
2023-10-19 11:43:29 -0400

    PrimerComponete

---
+++ src/components/PrimerComponente.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import './Styles/PrimerComponente.css'
+ const PrimerComponente=()=>{
+    return(
+        <div className="contenedor_banner">
+         <img src={"../public/Banner _DDD.jpg "}/>
+        </div>
+
+    )
+ }
+export default PrimerComponente
